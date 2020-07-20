882329db22c22505e824f416b28f76bcbedec661
Boris Chen <borischen@gamasys.com.tw>
2020-07-20 16:31:09 +0800

    add mb-2

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     const useNodeJS = true;   // if you are not using a node server, set this value to false
     const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    // const defaultLiffId = "1654440838-qXmvA08K";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
